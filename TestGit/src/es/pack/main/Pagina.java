faa5d2d2603a7fe15daca4490b7a2ba29a02721f
dimove <hector.dmv@outlook.es>
2017-05-13 19:33:38 +0200

    Update Estructura Pagina

--- TestGit/src/es/pack/main/Pagina.java
+++ TestGit/src/es/pack/main/Pagina.java
@@ -2,4 +2,23 @@ package es.pack.main;
 
 public class Pagina {
 
+	
+	public void onLoad(){
+		System.out.println("Hola caracola.");
+		
+		cargarDatos();
+	}
+
+	private void cargarDatos() {
+
+		
+		buscarDatosSesion();
+		
+	}
+
+	private void buscarDatosSesion() {
+		
+		
+	}
+	
 }
